19de064c605af0ebbb6cd68c2d6ed0ea861c9b82
guoxinjie <123456789>
2017-01-09 18:30:35 +0800

    火影忍者开发

--- naruto-api/src/main/java/com/vivion/naruto/NarutoService.java
+++ naruto-api/src/main/java/com/vivion/naruto/NarutoService.java
@@ -3,7 +3,6 @@ package com.vivion.naruto;
 import com.vivion.naruto.bean.Member;
 import com.vivion.naruto.enums.MemberParamter;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
