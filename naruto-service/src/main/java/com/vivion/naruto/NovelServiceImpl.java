ded7e229bdce50c414f088d13bf0fe44102451cf
guoxinjie <123456789>
2017-01-22 18:04:45 +0800

    小队分配功能新增

--- naruto-service/src/main/java/com/vivion/naruto/NovelServiceImpl.java
+++ naruto-service/src/main/java/com/vivion/naruto/NovelServiceImpl.java
@@ -5,7 +5,8 @@ import com.jspider.*;
 import com.jspider.pipeline.Pipeline;
 import com.jspider.processor.PageProcessor;
 import com.jspider.scheduler.QueueScheduler;
-import com.vivion.naruto.dao.MemberNinjaDao;
+import com.vivion.naruto.bean.NovelJsxs;
+import com.vivion.naruto.dao.NovelJsxsDao;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * Created by GuoXinJie on 2017-01-13.
@@ -23,14 +25,14 @@ public class NovelServiceImpl implements NovelService {
     private static Logger logger = LoggerFactory.getLogger(NovelServiceImpl.class);
 
     @Resource
-    private MemberNinjaDao memberNinjaDao;
+    private NovelJsxsDao novelJsxsDao;
 
 
     @Override
     public ResultEx getNovel(String date) {
         ResultEx resultEx =new ResultEx();
         try {
-            SpiderConfig spiderConfig = SpiderConfig.create("note", 50);
+            SpiderConfig spiderConfig = SpiderConfig.create("note", 5);
             SiteConfig siteConfig = SiteConfig.create().putCharset("aoyuge", "gbk");
 
             Spider spider = Spider.create(spiderConfig, siteConfig, new NotePageProcessor())
@@ -48,17 +50,28 @@ public class NovelServiceImpl implements NovelService {
     }
 
     private static class NotePipeline implements Pipeline {
+        @Resource
+        private NovelJsxsDao novelJsxsDao;
         @Override
         public void persist(Request request, Result result) {
+            NovelJsxs novelJsxs =new NovelJsxs();
             for (String name: result.keySet()) {
                 if (name.equals("title")){
-
+                    String text = result.get(name).toString().replaceAll("正文 ","").replaceAll("免费章节","").replaceAll("收费章节 ","");
+                    novelJsxs.setTitle(text);
                 }
                 if (name.equals("content")){
-
+                    String text = result.get(name).toString().replaceAll("&nbsp;&nbsp;&nbsp;&nbsp;", "").replaceAll("<br />\\n", "").replaceAll("    ", "")
+                            .replaceAll("<br />", "").replaceAll("<div id=\"BookText\">", "").replaceAll("</div>", "").replaceAll("【★不要忘记点击本吧右上角的“签到”★】","")
+                            .replaceAll("【欢迎大家到3g为犀利增加点击量，投推荐票。】","").replaceAll("——","").replaceAll("《绝世邪神》最近100章连载贴","")
+                            .replaceAll("《绝世邪神》全部章节连载贴","").replaceAll("〖傲宇阁小说网∷更新快∷无弹窗∷纯文字∷ ..〗","");
+                    novelJsxs.setContent(text);
                 }
+                novelJsxs.setIsValid(1);
+                novelJsxs.setCreateTime(new Date());
             }
-            System.out.println(result);
+            novelJsxsDao.add(novelJsxs);
+            //System.out.println(result);
         }
     }
 
